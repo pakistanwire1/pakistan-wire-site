a3543dbc1de31f320c7667c5a98742f15d452930
pakistanwire1 <pakistanwire1@gmail.com>
2025-11-30 08:58:16 +0500

    git commit -m
    add

---
+++ script.js
@@ -0,0 +1,5 @@
+// Example: basic form validation / message pop
+document.querySelector('.contact-form').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('Your message has been sent!');
+});
